13d32e89d8eebb97d5a1918f88d2cbb8fa0d513e
Ishan Gaur <ishangaur4@gmail.com>
2023-11-29 20:10:55 -0800

    initial experiment with sentences with all or no caps and temperature variation

--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
+import math
 import json
 from warnings import warn
+from pathlib import Path
+from random import shuffle
+from pprint import pformat
+from openai import OpenAI
+from config import api_key, seed
+
+client = OpenAI(api_key=api_key)
+results_path = Path.cwd() / "results"
+if not results_path.exists():
+    results_path.mkdir()
 
 def load_dataset(dataset_file):
     dataset = json.load(open(dataset_file, "r"))
@@ -36,8 +47,106 @@ def create_dataset(dataset, tags_true, tags_false=[], tags_common=[], default_me
                 warn(f"Invalid default method: {default_method}, must be in {default_methods}")
                 continue
         if positive:
+            sample["label"] = True 
             positives.append(sample)
         elif negative:
+            sample["label"] = False
             negatives.append(sample)
 
     return positives, negatives
+
+
+prompt_prefix = ""
+
+def in_context_from_samples(samples, tile=True):
+    keys = ["input", "label"]
+    positives = [samples[i] for i in range(len(samples)) if samples[i]["label"]]
+    negatives = [samples[i] for i in range(len(samples)) if not samples[i]["label"]]
+    prompt_order = []
+    if tile:
+        for p, n in zip(positives, negatives):
+            prompt_order.append(p)
+            prompt_order.append(n)
+    else:
+        prompt_order = positives + negatives
+
+    prompt_samples = []
+    for sample in prompt_order:
+        prompt_samples.append({key: sample[key] for key in keys})
+
+    prompt = prompt_prefix + pformat(prompt_samples)
+    return prompt
+
+user_prefix = ""
+def test_prompt_from_samples(positives, negatives):
+    samples = positives + negatives
+    shuffle(samples)
+    prompt_samples = [{"input": sample["input"]} for sample in samples]
+    prompt_labels = [sample["label"] for sample in samples]
+    return user_prefix + pformat(prompt_samples), samples
+
+json_prefix = "Please label the following inputs. Respond in JSON format like the examples given to you above."
+def gpt_prediction(system_prompt, user_query, model="gpt-4", temperature=1.0, json_mode=False):
+    if json_mode and model != "gpt-4-1106-preview":
+        warn("json_mode only supported for gpt-4-1106-preview")
+        warn("changing model to gpt-4-1106-preview")
+        model = "gpt-4-1106-preview"
+    if json_mode and "json" not in user_query.lower():
+        warn("json_mode is on but user_query does not contain \"json\"")
+        warn(f"adding prefix to user_query:\n\"{json_prefix}\"")
+        user_query = json_prefix + user_query
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": user_query
+            }
+        ],
+        temperature=1,
+        max_tokens=math.floor(1.1 * len(user_query)),
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        response_format={"type": "json_object" if json_mode else "text"},
+        seed=seed
+    )
+    # check if a response is an error
+    # if response
+    if response.choices[0].finish_reason == "length":
+        warn("Response reached max tokens, consider increasing max_tokens")
+
+    response_json = json.loads(response.choices[0].message.content)
+    if type(response_json) != list:
+        response_json = response_json[list(response_json.keys())[0]]
+    if type(response_json) != list:
+        warn("Response is not a list")
+
+    return response_json
+
+def eval_response(response_json, test_samples):
+    eval_results = []
+    correct, mismatch, incorrect = 0, 0, 0
+    for r_sample, t_sample in zip(response_json, test_samples):
+        eval_results.append(t_sample)
+        eval_results[-1]["mismatch"] = r_sample["input"] != t_sample["input"]
+        eval_results[-1]["eval"] = ((r_sample["label"] == t_sample["label"]) and not eval_results[-1]["mismatch"])
+        correct += eval_results[-1]["eval"]
+        mismatch += eval_results[-1]["mismatch"] and eval_results[-1]["eval"]
+        incorrect += not eval_results[-1]["eval"]
+    if correct + mismatch + incorrect != len(test_samples):
+        warn("Something went wrong with eval_response. \
+             The sum of correct, mismatch, and incorrect should equal the number of test samples.")
+    summary_dict = {
+        "correct": correct,
+        "mismatch": mismatch,
+        "incorrect": incorrect,
+        "total": len(test_samples),
+        "accuracy": correct / len(test_samples)
+    }
+    return eval_results, summary_dict
